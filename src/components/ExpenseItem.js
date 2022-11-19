a964acd8486002f00aed40950a5bd1c27a4df6cf
Dimos Maginas <dimos.maginas@adesso.de>
2022-11-19 04:56:04 +0100

    Added styling to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,11 @@
+import './ExpenseItem.css';
 
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    return <div className='expense-item'>
+        <div>Date</div>
+        <div className="expense-item__description"><h2>Title</h2></div>
+        <div className="expense-item__price">$199.99</div>
+    </div>
 }
 
 export default ExpenseItem;
