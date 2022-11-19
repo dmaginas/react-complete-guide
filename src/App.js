0c55b9ac006fe020071eca0d6a76e4d5ac1e4f6d
Dimos Maginas <dimos.maginas@adesso.de>
2022-11-19 12:37:38 +0100

    Exercise 3/Working with Lists

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPS = [
+  {
+    id: '1',
+    title: 'Toilettenpapier',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: '2',
+    title: 'Neuer Fernseher',
+    amount: 799.49,
+    date: new Date(2021, 2, 12) },
+  {
+    id: '3',
+    title: 'KfZ-Versicherung',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: '4',
+    title: 'Neuer Schreibtisch (aus Holz)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: '1',
-      title: 'Toilettenpapier',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: '2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: '3',
-      title: 'KfZ-Versicherung',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: '4',
-      title: 'Neuer Schreibtisch (aus Holz)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPS);
 
   const addExpenseHandler = expense => {
-    console.log(expense);
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
   };
 
   return (
