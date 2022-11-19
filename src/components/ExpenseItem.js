226ac62bcaa13a2af7bc5efdeea5d59ea69ae26d
Dimos Maginas <dimos.maginas@adesso.de>
2022-11-19 05:20:56 +0100

    Added normal javascript logic into ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(data) {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('de-DE', { month: 'long' });
+    const day = props.date.toLocaleString('de-DE', { day: '2-digit' });
+    const year = props.date.getFullYear();
+  
     return (
       <div className='expense-item'>
-        <div>{data.date.toISOString()}</div>
+        <div>
+          <div>{day}</div>
+          <div>{month}</div>
+          <div>{year}</div>
+        </div>
         <div className='expense-item__description'>
-          <h2>{data.title}</h2>
-          <div className='expense-item__price'>${data.amount}</div>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
         </div>
       </div>
     );
   }
-
 export default ExpenseItem;
