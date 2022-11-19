722ddf65b02269a590d7092002956776e66b0416
Dimos Maginas <dimos.maginas@adesso.de>
2022-11-19 14:03:39 +0100

    Added Chart/ChartBar components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,9 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  
   const [title, setTitle] = useState(props.title);
     
-  const clickHandler = () => {
-    setTitle('Updated the item!');
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -18,7 +15,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
