2542a20cefc814daf459b3ba101d49d8dbec5e26
Dimos Maginas <dimos.maginas@adesso.de>
2022-11-19 05:12:24 +0100

    Passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    return <div className='expense-item'>
-        <div>Date</div>
-        <div className="expense-item__description"><h2>Title</h2></div>
-        <div className="expense-item__price">$199.99</div>
-    </div>
-}
+function ExpenseItem(data) {
+    return (
+      <div className='expense-item'>
+        <div>{data.date.toISOString()}</div>
+        <div className='expense-item__description'>
+          <h2>{data.title}</h2>
+          <div className='expense-item__price'>${data.amount}</div>
+        </div>
+      </div>
+    );
+  }
 
 export default ExpenseItem;
